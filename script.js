fcaca141bc4bfb42347797eb18d43d5fa07c6c30
santiagodev <sanxp18@gmail.com>
2024-09-23 18:28:18 -0400

    fetch data from data.json file

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
+// En tu segundo script
+fetch('./data.json')
+    .then(response => response.json()) // Analiza el contenido JSON
+    .then(data => {
+        // Ahora "data" es un objeto JavaScript con los datos del JSON
+        // console.log(data[1].name); // Imprime el nombre del primer postre
+        console.log(data); // Imprime el precio del segundo postre
+        for (const property in data) {
+            console.log(data[property].image.thumbnail);
+            const li = document.createElement("li");
+            const img = document.createElement("img");
+            const productDescription = document.createElement("div");
+            const buttonContainer = document.createElement("div");
+
+            ul.appendChild(li);
+            li.append(img);
+            img.src = data[property].image.thumbnail;
+        }
+        // ... y así sucesivamente
+    })
+    .catch(error => console.error('Error al cargar el archivo JSON:', error));
+
+
+const ul = document.querySelector("ul");
 const productImage = document.querySelector(".product-image");
 const addToCartButton = document.querySelector(".add-remove-to-cart-container");
-const cartIcon = document.querySelector(".cart-icon");
-const cartText = document.querySelector(".cart-text");
+const cartIcon = document.querySelector(".add-cart__icon");
+const cartText = document.querySelector(".add-cart__text");
 const cart = document.querySelector(".cart");
 
 addToCartButton.addEventListener("click", addItemToCart, {once: true})
 
 function addItemToCart(event) {
     console.log(event)
+    /*Primero se ponen los estilos para el producto seleccionado*/
+
+    /*Eliminando los elementos que lleva por defecto el add to cart*/
     cartIcon.remove();
     cartText.remove();
 
-    /*poner estilos al borde de la imagen y al background del boton*/
+    /*poniendo estilos al borde de la imagen y al background del boton*/
     productImage.classList.add("selected-product");
     addToCartButton.classList.add("add-to-cart--selected");
     /*creando los botones y el span*/
@@ -44,4 +71,6 @@ function addItemToCart(event) {
     addToCartButton.appendChild(minusButton);
     addToCartButton.appendChild(productCounter);
     addToCartButton.appendChild(plusButton);
+
+
 }
